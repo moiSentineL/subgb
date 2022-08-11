61c0d18cc85c43ab404c8e93b59b0d1654c369e8
moisentinel <nibir.patowary@gmail.com>
2022-08-11 12:57:16 +0530

    v2.0

--- notification.py
+++ notification.py
@@ -1,15 +1,20 @@
 import requests
+import configparser
+
+# ConfigParser to parse ini
+config = configparser.ConfigParser()
+config.read('notif.ini')
+
+# Telegram Bot Credentials
+bot_token = config.get('Credentials', 'token')
+bot_chatID = config.get('Credentials', 'chatID')
+
+title= config.get('Pattern', 'title')
+message= config.get('Pattern', 'message')
 
 def send(bot_message):
     
-    bot_token = '5551558898:AAHjJxvOgmoxKKjNhvSizqKggrEE1Kvj0qk'
-    bot_chatID = '1316756456'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
-    response = requests.get(send_text)
-
-    return response.json()
     
-
-# test = send("*moanarr*\nTesting Telegram bot")
-# print(test)
+    response = requests.get(send_text)
+    return response.json()
--- sub.py
+++ sub.py
@@ -1,35 +1,50 @@
-import os, shutil, reverse, notification
+import os, shutil, reverse, notification, sys, re
 
+from tkinter import filedialog
+from tkinter import *
 
-source = input('Source Folder: ')
-dest = input('Destination Folder: ')
 # Walk through all files in the directory that contains the files to copy
 
-show = input('\nEnter Show name with year: ')
-season = input('Season: ')
-splitter = input('Select index position: ')
+def rename_and_move(source, dest):
+    global show, season
+    show = input('\nEnter Show name with year: ')
+    season = input('Season: ')
 
-for root, dirs, files in os.walk(source):
+    for root, dirs, files in os.walk(source):
 
-    for filename in files:
+        for filename in files:
 
-        if filename == '2_English.srt':
-            base = os.path.join(os.path.abspath(root))
-            #Get current name
-            old_name = os.path.join(base, filename)
-            #Get parent folder
-            parent_folder = os.path.basename(base)
-            #Get episode name from folder
-            episode = parent_folder.split(".")[int(splitter)]
-            new_file_name = show + ' ' + episode + ".en.srt" #assuming same extension
-            # print(new_file_name)
-            new_abs_name = os.path.join(base, new_file_name) 
-            # #Rename to new name
-            os.rename(old_name,new_abs_name)
-            # #Copy to destination
-            # # one_level_up = os.path.normpath(os.path.join(base, os.pardir))
-            dest_name = os.path.join(dest, new_file_name)
-            shutil.copy(new_abs_name,dest_name)
+            if filename == '2_English.srt':
+                base = os.path.join(os.path.abspath(root))
+                #Get current name
+                old_name = os.path.join(base, filename)
+                #Get parent folder
+                parent_folder = os.path.basename(base)
+                #Get episode name from folder
+                episode = re.findall(r"(S\d{2}E\d{2})", parent_folder)
+                new_file_name = show + ' ' + episode[0] + ".en.srt" #assuming same extension
+                # print(new_file_name)
+                new_abs_name = os.path.join(base, new_file_name) 
+                # #Rename to new name
+                os.rename(old_name,new_abs_name)
+                # #Copy to destination
+                dest_name = os.path.join(dest, new_file_name)
+                shutil.copy(new_abs_name,dest_name)
 
-reverse.reversal(source, show, splitter)
-# notification.send(f'*Moanarr*\nFinished setting up subtitles for {show}; Season: {season}')
+root = Tk()
+root.withdraw()
+
+source = filedialog.askdirectory(title="Select Subtitle Folder")
+dest = filedialog.askdirectory(title="Select Destination Folder")
+
+try:
+    rename_and_move(source, dest)
+    reverse.reversal(source, show)
+    
+    if '&' in show:
+        notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show.replace('&', '%26')}*")
+    else:
+        notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show}*")
+except:
+    print('An error occurred. Exiting...')
+    sys.exit()
