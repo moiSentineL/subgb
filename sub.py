aaef097afef9895b509030f30ca10273d88afe2e
moisentinel <nibir.patowary@gmail.com>
2022-08-21 09:57:44 +0530

    v3.0

--- sub.py
+++ sub.py
@@ -1,3 +1,9 @@
+# Subgb v3.0
+# Source Code
+# Developer: moiSentineL (www.github.com/moiSentineL)
+# Website: https://github.com/moiSentineL/subgb
+
+
 import os, shutil, reverse, notification, sys, re
 
 from tkinter import filedialog
@@ -7,8 +13,8 @@ from tkinter import *
 
 def rename_and_move(source, dest):
     global show, season
-    show = input('\nEnter Show name with year: ')
-    season = input('Season: ')
+    show = showentry.get()
+    season = seasonentry.get()
 
     for root, dirs, files in os.walk(source):
 
@@ -31,20 +37,73 @@ def rename_and_move(source, dest):
                 dest_name = os.path.join(dest, new_file_name)
                 shutil.copy(new_abs_name,dest_name)
 
+def find_source():
+    global source
+    source = filedialog.askdirectory(title="Select Subtitle Folder")
+
+def find_dest():
+    global dest
+    dest = filedialog.askdirectory(title="Select Destination Folder")
+
+def proceed():
+    try:
+        rename_and_move(source, dest)
+        reverse.reversal(source, show)
+        
+        if '&' in show:
+            notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show.replace('&', '%26')}*")
+        else:
+            notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show}*")
+    except:
+        print('An error occurred. Exiting...')
+        sys.exit()
+
 root = Tk()
+
+# Labels
+    # Show Label
+l1 = Label(root, text = "Show:", font=("Arial",12,'bold')) 
+l1.grid(row = 1, column = 1, sticky = W, pady= 15, padx = 5)
+    # Show Entry
+showentry = Entry(root, width= 30, bd=3)
+showentry.grid(row = 1, column = 2, pady = 2,columnspan=2, sticky=E)
+showentry.focus()
+    # Season label
+l2 = Label(root, text = "Season:", font=("Arial",12,'bold')) 
+l2.grid(row = 2, column = 1, sticky = W,  padx = 5)
+    # Season Entry
+seasonentry = Entry(root, width= 5, bd=3) 
+seasonentry.grid(row = 2, column = 2, sticky=W)
+
+# Buttons
+    # Button to open log file.
+sourcebut = Button(root, text= "Source", command=find_source, width= 8)
+sourcebut.grid(row= 2, column=3, sticky= E, padx= 5)
+
+    # Button to delete log file.
+destbut = Button(root, text= "Destination",  command=find_dest, width= 10)
+destbut.grid(row= 2, column= 4, sticky= E, padx= 5)
+
+    # Button to proceed
+proceedbut = Button(root, text= "Proceed",  command=proceed, width= 8)
+proceedbut.grid(row = 1, column = 4, sticky = E, padx= 5)
+
+# Tkinter settings
+window_height = 100
+window_width = 440
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_cordinate = int((screen_width/2) - (window_width/2))
+y_cordinate = int((screen_height/2.5) - (window_height/2))
+# (RECOMMENDED TO NOT TOUCH) Window Metadeta.
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+root.title('Subgb')
+root.resizable(False, False)
+# root.iconbitmap('thoughtlogger.ico')
+root.tk.call('tk', 'scaling', 2.0)
+
+root.mainloop()
 root.withdraw()
 
-source = filedialog.askdirectory(title="Select Subtitle Folder")
-dest = filedialog.askdirectory(title="Select Destination Folder")
 
-try:
-    rename_and_move(source, dest)
-    reverse.reversal(source, show)
-    
-    if '&' in show:
-        notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show.replace('&', '%26')}*")
-    else:
-        notification.send(f"*{notification.title}*\n{notification.message}\n*Season {season}* of *{show}*")
-except:
-    print('An error occurred. Exiting...')
-    sys.exit()
+# End of the code
